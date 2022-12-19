a7328da3b82e20b5ad9b0209605a2069c956b99a
Shourya54S <120584394+Shourya54S@users.noreply.github.com>
2022-12-19 19:35:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+song = "";
+song1 = "";
+function preload(){
+song = loadSound("song1.mp3");
+song1 = loadSound("song2.mp3")
+}
+function draw(){}  
+function play(){
+song.play();    
+}
+function stop(){
+song.stop();    
+}
+function play1(){
+song1.play();    
+}
+function stop1(){
+song1.stop();    
+}
